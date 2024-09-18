20e55b185452ae569c66bc0618fd715d6948b48d
C00psta <clilly1@uwyo.edu>
2024-09-18 00:57:14 +0000

    final

--- cosc1010-lab02.py
+++ cosc1010-lab02.py
@@ -1,41 +1,49 @@
-# Your Name Here
+# Cooper Lilly
 # UWYO COSC 1010
-# Submission Date
+# 9/17/2024
 # Lab 02 
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# Lab Section: 13 worked in 14
+# Sources, people worked with, help given to: none
 # your
 # comments
 # here
 
-your_variable_here = "when you see this, replace it with your code"
+hello_message = "hello, COSC1010"
 
 ## Section ONE
 
 # Complete the following print statement to print out "Hello, COSC1010"
-print()
+print("Hello, COSC1010")
 
 # Assign the string above to a variable named hello_message and print that variable
-hello_message = ""
-print("Your code here")
+hello_message = "Hello, COSC1010"
+print(hello_message)
 
 # Assign the string "cowboy joe" to a variable, and print that variable with title casing
-print("Your code here")
+go_pokes = "cowboy joe"
+print(go_pokes.title())
 
 # Complete the following f-string print message 
     # You will need to create your own variables and insert them  
     # the final message should read `The University of Wyoming was founded in 1886`
-
-print(f"The {your_variable_here} was founded in {your_variable_here}")
+uni = "University of Wyoming"
+year = 1886
+print(f"The {uni} was founded in {year}")
 
 # Now let's do some math with variables 
     # Create two variables x and y and assign them the values 5 and 10 respectively 
     # Complete the following print statements using your variables
     #All math must be done within the braces in the f-strings
-print(f"x + y = {your_variable_here}")
-print(f"x - y = {your_variable_here}")
-print(f"x * y = {your_variable_here}")
-print(f"x / y = {your_variable_here}")
+x = 5
+y = 10
+a = 15
+b = -5
+c = 50
+d = 0.5
+print(f"x + y = {a}")
+print(f"x - y = {b}")
+print(f"x * y = {c}")
+print(f"x / y = {d}")
 
 # String concatenation 
     # Finally we will take a look at string concatenation
@@ -46,3 +54,7 @@ print(f"x / y = {your_variable_here}")
         # last_name, which is your last name
         # space, which is a space character 
     # Use string concatenation to print out your full name 
+first_name = "Cooper"
+last_name = "Lilly"
+space = " "
+print(first_name+space+last_name)
